7769954515f303b6ec2b67c1f03348aa0a809b3d
Andrew Dawson <andrewjdawson2016@gmail.com>
2023-11-05 15:51:31 -0800

    start using router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import AddLeaseForm from "./AddLeaseForm";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
@@ -9,7 +15,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AddLeaseForm />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/add-lease" />
+          </Route>
+          <Route path="/add-lease">
+            <AddLeaseForm />
+          </Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
