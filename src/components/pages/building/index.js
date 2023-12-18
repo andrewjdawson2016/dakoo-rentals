1d58db56a15713520b0b623d13ff29c7abae15e2
Andrew Dawson <andrewjdawson2016@gmail.com>
2023-12-18 14:20:06 -0800

    enable access stub of building page

--- src/components/pages/building/index.js
+++ src/components/pages/building/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import TopLevelToolbar from "../../common/TopLevelToolbar";
-import { AppBar, Container } from "@mui/material";
+import { AppBar, Container, CircularProgress } from "@mui/material";
 import Header from "./Header";
 import BuildingToolbar from "./BuildingToolbar";
 import { getBuilding } from "../../../api";
@@ -10,18 +11,36 @@ export function BuildingPage() {
   const { building_id } = useParams();
 
   const refreshBuilding = () => {
-    getBuilding(building_id)
-      .then((building) => {
-        setBuilding(building);
-      })
-      .catch((error) => {
-        console.error("Failed to fetch building:", error);
-      });
+    if (building_id) {
+      getBuilding(building_id)
+        .then((building) => {
+          setBuilding(building);
+        })
+        .catch((error) => {
+          console.error("Failed to fetch building:", error);
+        });
+    }
   };
 
   useEffect(() => {
     refreshBuilding();
-  }, []);
+  }, [building_id]);
+
+  if (!building) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
+
   return (
     <>
       <AppBar position="static" sx={{ backgroundColor: "#f5f5f5" }}>
