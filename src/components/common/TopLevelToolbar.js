893301295aaf75a328140af802381d5a04d9e6ec
Andrew Dawson <andrewjdawson2016@gmail.com>
2023-12-29 15:48:34 -0800

    add logout button

--- src/components/common/TopLevelToolbar.js
+++ src/components/common/TopLevelToolbar.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Toolbar, Typography } from "@mui/material";
+import { Link, useHistory } from "react-router-dom";
+import { Toolbar, Typography, Button } from "@mui/material";
+import { logout } from "../../api";
 
 function TopLevelToolbar() {
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      history.push("/login");
+    } catch (e) {
+      console.error("Logout failed: ", e);
+    }
+  };
+
   return (
     <Toolbar>
       <Typography
@@ -14,6 +26,9 @@ function TopLevelToolbar() {
           <img src="/logo.png" alt="DaKoo Rentals" style={{ height: "50px" }} />
         </Link>
       </Typography>
+      <Button color="inherit" onClick={handleLogout}>
+        Logout
+      </Button>
     </Toolbar>
   );
 }
