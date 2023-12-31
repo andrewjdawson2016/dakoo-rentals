8f0373734ec206cbfe0c4f713b4ca94c247bb7f2
Andrew Dawson <andrewjdawson2016@gmail.com>
2023-12-30 21:01:05 -0800

    Revert "get the logout flow correct"
    This reverts commit 4567485ed988eec54d7246a7b090533a79b64d67.

--- src/components/common/TopLevelToolbar.js
+++ src/components/common/TopLevelToolbar.js
@@ -1,32 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import {
-  Toolbar,
-  Typography,
-  IconButton,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-  Divider,
-  Box,
-} from "@mui/material";
-import PersonIcon from "@mui/icons-material/Person";
+import { Toolbar, Typography, Button } from "@mui/material";
 import { logout } from "../../api";
 
 function TopLevelToolbar({ user }) {
   const navigate = useNavigate();
-  const [drawerOpen, setDrawerOpen] = useState(false);
-
-  const toggleDrawer = (open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-    setDrawerOpen(open);
-  };
 
   const handleLogout = async () => {
     try {
@@ -36,33 +14,6 @@ function TopLevelToolbar({ user }) {
       console.error("Logout failed: ", e);
     }
   };
-
-  const sideDrawer = (
-    <Box
-      role="presentation"
-      onClick={toggleDrawer(false)}
-      onKeyDown={toggleDrawer(false)}
-      sx={{ width: 250 }}
-    >
-      <List>
-        <ListItem sx={{ cursor: "default", userSelect: "none" }}>
-          <ListItemText primary={`${user.firstName} ${user.lastName}`} />
-        </ListItem>
-      </List>
-      <Divider />
-      <List>
-        <ListItem
-          onClick={handleLogout}
-          sx={{ cursor: "pointer", userSelect: "none" }}
-        >
-          <ListItemText
-            primary="Logout"
-            primaryTypographyProps={{ style: { color: "red" } }}
-          />
-        </ListItem>
-      </List>
-    </Box>
-  );
 
   return (
     <Toolbar>
@@ -75,12 +26,9 @@ function TopLevelToolbar({ user }) {
           <img src="/logo.png" alt="DaKoo Rentals" style={{ height: "50px" }} />
         </Link>
       </Typography>
-      <IconButton color="primary" onClick={toggleDrawer(true)}>
-        <PersonIcon />
-      </IconButton>
-      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
-        {sideDrawer}
-      </Drawer>
+      <Button color="primary" onClick={handleLogout}>
+        Logout
+      </Button>
     </Toolbar>
   );
 }
