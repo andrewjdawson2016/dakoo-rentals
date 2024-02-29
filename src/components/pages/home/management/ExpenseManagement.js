378885f3b3884cc416dec1337c6251ad5ad88a97
Andrew Dawson <andrewjdawson2016@gmail.com>
2024-02-29 05:01:17 -1000

    clean up unused imports

--- src/components/pages/home/management/ExpenseManagement.js
+++ src/components/pages/home/management/ExpenseManagement.js
@@ -1,36 +1,8 @@
-import React, { useState, useEffect } from "react";
-import {
-  Typography,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  IconButton,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  TextField,
-  DialogActions,
-  Button,
-  Snackbar,
-  Box,
-} from "@mui/material";
-import CheckIcon from "@mui/icons-material/Check";
-import CloseIcon from "@mui/icons-material/Close";
-import {
-  getEventsInRange,
-  formatDate,
-  mapEventDescription,
-} from "../../../../util";
-import { updateLeaseEventExecutionDate } from "../../../../api";
-import { DateTime, Interval } from "luxon";
+import React from "react";
+import { Typography } from "@mui/material";
 
-function EventManager({ buildings, refreshBuildings }) {
-  return <Typography variant="h6">No upcoming events.</Typography>;
+function ExpenseManagement({ buildings, refreshBuildings }) {
+  return <Typography variant="h6">Coming Soon...</Typography>;
 }
 
-export default EventManager;
+export default ExpenseManagement;
