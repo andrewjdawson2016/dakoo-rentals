a84f0d5dd9125369afe67c413e5fa038fd113fa3
Andrew Dawson <andrewjdawson2016@gmail.com>
2024-02-29 16:47:46 -1000

    a few cleanups to expense logging

--- src/components/pages/home/management/ExpenseManagement.js
+++ src/components/pages/home/management/ExpenseManagement.js
@@ -9,31 +9,31 @@ import {
   TableRow,
   IconButton,
   Dialog,
+  TextField,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Snackbar,
+  DialogContentText,
 } from "@mui/material";
 import { Add } from "@mui/icons-material";
-import { getExpenseMonths, formatDateToMonthYear } from "../../../../util";
+import {
+  getExpenseMonths,
+  formatDateToMonthYear,
+  parseAndFormatMonthlyMoneyValue,
+} from "../../../../util";
 import { DateTime } from "luxon";
-import LogExpenseForm from "./LogExpenseForm";
+import { createExpense } from "../../../../api";
 
 function ExpenseManagement({ buildings, refreshBuildings }) {
   const [dialogOpen, setDialogOpen] = useState(false);
   const [selectedMonthYear, setSelectedMonthYear] = useState(null);
   const [selectedBuilding, setSelectedBuilding] = useState(null);
-
-  const handleCloseDialog = () => {
-    setDialogOpen(false);
-  };
-
-  const onSubmitSuccess = () => {
-    handleCloseDialog();
-    refreshBuildings();
-  };
-
-  const handleAddExpense = (building, month) => {
-    setSelectedBuilding(building);
-    setSelectedMonthYear(month);
-    setDialogOpen(true);
-  };
+  const [formData, setFormData] = useState({ amount: "", note: "" });
+  const [displayedAmount, setDisplayedAmount] = useState("");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
 
   const expensesData = buildings.flatMap((building) =>
     getExpenseMonths(
@@ -44,6 +44,54 @@ function ExpenseManagement({ buildings, refreshBuildings }) {
       .filter(([_, expense]) => !expense)
       .map((data) => ({ ...data, building }))
   );
+
+  const handleAddExpense = (building, month) => {
+    setSelectedBuilding(building);
+    setSelectedMonthYear(month);
+    setDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+    setSelectedBuilding(null);
+    setSelectedMonthYear(null);
+    setFormData({ amount: "", note: "" });
+    setDisplayedAmount("");
+  };
+
+  const handleAmountChange = (event) => {
+    const { numericValue, formattedValue } = parseAndFormatMonthlyMoneyValue(
+      event.target.value
+    );
+    setFormData((prev) => ({ ...prev, amount: numericValue }));
+    setDisplayedAmount(formattedValue);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = () => {
+    const dataToSend = {
+      building_id: selectedBuilding.id,
+      month_year: selectedMonthYear,
+      ...formData,
+    };
+
+    createExpense(dataToSend)
+      .then(() => {
+        setSnackbarMessage("Expense created successfully");
+        setSnackbarOpen(true);
+        handleCloseDialog();
+        refreshBuildings();
+      })
+      .catch((error) => {
+        console.error("Error creating expense:", error);
+        setSnackbarMessage(`Error creating expense: ${error.message}`);
+        setSnackbarOpen(true);
+      });
+  };
 
   return (
     <>
@@ -62,7 +110,7 @@ function ExpenseManagement({ buildings, refreshBuildings }) {
               const month = data[0];
               return (
                 <TableRow key={index}>
-                  <TableCell>{building.nickname}</TableCell>{" "}
+                  <TableCell>{building.nickname}</TableCell>
                   <TableCell>{formatDateToMonthYear(month)}</TableCell>
                   <TableCell>
                     <IconButton
@@ -77,13 +125,49 @@ function ExpenseManagement({ buildings, refreshBuildings }) {
           </TableBody>
         </Table>
       </TableContainer>
+
       <Dialog open={dialogOpen} onClose={handleCloseDialog}>
-        <LogExpenseForm
-          building={selectedBuilding}
-          monthYear={selectedMonthYear}
-          onSubmitSuccess={onSubmitSuccess}
-        />
+        <DialogTitle>{selectedBuilding?.nickname}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Expense for {formatDateToMonthYear(selectedMonthYear)}{" "}
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="amount"
+            label="Amount"
+            name="amount"
+            value={displayedAmount}
+            onChange={handleAmountChange}
+            fullWidth
+            required
+          />
+          <TextField
+            margin="dense"
+            id="note"
+            label="Optional Note"
+            name="note"
+            value={formData.note}
+            onChange={handleChange}
+            fullWidth
+            multiline
+            rows={4}
+          />
+        </DialogContent>
+        <DialogActions style={{ justifyContent: "center" }}>
+          <Button onClick={handleSubmit} variant="contained" color="primary">
+            Submit
+          </Button>
+        </DialogActions>
       </Dialog>
+
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
+        message={snackbarMessage}
+      />
     </>
   );
 }
